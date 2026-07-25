3086196324f834f33de51aee6ef720d83da853e3
GeRRy <gerritkirchner1@outlook.de>
2026-07-26 01:24:00 +0200

    Implemented Watchdog Task Monitoring and integrated Watchdog reset handling into the Main application.

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "myDebug/debug.h"
 #include "myWatchdog/watchdog.h"
+#include "esp_task_wdt.h"
 
 static const char *TAG = "DEBUG-MAIN";
 
@@ -9,4 +10,10 @@ extern "C" void app_main(void)
     ESP_LOGI(TAG, "Main started");
     debug_init();
     watchdog_init();
+
+     while (true)
+    {
+        esp_task_wdt_reset();
+        vTaskDelay(pdMS_TO_TICKS(1000));  
+    }
 }
--- main/myWatchdog/watchdog.cpp
+++ main/myWatchdog/watchdog.cpp
@@ -2,8 +2,25 @@
 #include "myDebug/debug.h"
 #include "myConfig/config.h"
 #include "esp_task_wdt.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 static const char *TAG = "DEBUG-WATCHDOG";
+static TaskHandle_t watchdog_task_handle = nullptr;
+static bool watchdog_task_registered = false;
+
+static void watchdog_task(void *parameter)
+{
+    while (!watchdog_task_registered)
+    {
+        vTaskDelay(pdMS_TO_TICKS(10)); 
+    }
+    while (true)
+    {
+        esp_task_wdt_reset();
+        vTaskDelay(pdMS_TO_TICKS(1000));  
+    }  
+}
 
 void watchdog_init()
 {
@@ -29,5 +46,27 @@ void watchdog_init()
         DEBUG_ERROR(TAG, "Watchdog initialization failed: %s", esp_err_to_name(result));
         return;
     }
+    esp_task_wdt_add(NULL);
+    if (result != ESP_OK)
+    {
+        DEBUG_ERROR(TAG, "Failed to add app_main to Watchdog: %s", esp_err_to_name(result));
+        return;
+    }
+    xTaskCreate(
+        watchdog_task,
+        "watchdog_task",
+        2048,
+        nullptr,
+        5,
+        &watchdog_task_handle
+    );
+    esp_task_wdt_add(watchdog_task_handle);
+    if (result != ESP_OK)
+    {
+        DEBUG_ERROR(TAG, "Failed to add Watchdog Task to Watchdog: %s", esp_err_to_name(result));
+        return;
+    }
+    watchdog_task_registered = true;
+
     DEBUG_INFO(TAG, "Watchdog System initialized");
 }
