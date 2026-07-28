60c82ffc8e2be9245639f6a7e2f0a9f9c9ec1fd7
GeRRy <gerritkirchner1@outlook.de>
2026-07-28 20:09:47 +0200

    Optimized Watchdog Modul

--- main/myWatchdog/watchdog_tasks.cpp
+++ main/myWatchdog/watchdog_tasks.cpp
@@ -14,11 +14,11 @@ static TaskHandle_t debug_task_handle = nullptr;
 
 extern bool debug_task_registered = false;
 
-void watchdog_tasks_init()
-{
-    /*===========================================================
+ /*===========================================================
     app-main
     ===========================================================*/
+void main_task()
+{
     DEBUG_DEBUG(TAG, "Adding app_main Task to Watchdog System...." );
     esp_err_t result = esp_task_wdt_add(NULL);
     if (result != ESP_OK)
@@ -27,10 +27,13 @@ void watchdog_tasks_init()
         return;
     }
     DEBUG_INFO(TAG, "Adding app_main Task to Watchdog System successful" );
+}
 
-    /*===========================================================
+ /*===========================================================
     Watchdog-Modul
     ===========================================================*/
+void wachdog_task()
+{
     DEBUG_DEBUG(TAG, "Adding Watchdog Task to Watchdog System...." );
     xTaskCreate(
         watchdog_task,
@@ -40,7 +43,7 @@ void watchdog_tasks_init()
         5,
         &watchdog_task_handle
     );
-    esp_task_wdt_add(watchdog_task_handle);
+    esp_err_t result = esp_task_wdt_add(watchdog_task_handle);
     if (result != ESP_OK)
     {
         DEBUG_ERROR(TAG, "Failed to add Watchdog Task to Watchdog: %s", esp_err_to_name(result));
@@ -48,10 +51,13 @@ void watchdog_tasks_init()
     }
     watchdog_task_registered = true;
     DEBUG_INFO(TAG, "Adding Watchdog Task to Watchdog System successful" );
+}
 
-    /*===========================================================
+/*===========================================================
     Debug-Modul
     ===========================================================*/
+void debug_task()
+{
     if (!CONFIG_DEBUG_ENABLED)
     {
         DEBUG_DEBUG(TAG, "Debug Modul not Enabled");
@@ -66,7 +72,7 @@ void watchdog_tasks_init()
         5,
         &debug_task_handle
     );
-    esp_task_wdt_add(debug_task_handle);
+    esp_err_t result = esp_task_wdt_add(debug_task_handle);
     if (result != ESP_OK)
     {
         DEBUG_ERROR(TAG, "Failed to add Debug Task to Watchdog: %s", esp_err_to_name(result));
@@ -74,4 +80,40 @@ void watchdog_tasks_init()
     }
     debug_task_registered = true;
     DEBUG_INFO(TAG, "Adding Debug Task to Watchdog System successful" );
+}
+
+/*===========================================================
+    YOUcoolMODUL
+    ===========================================================*/
+/*void YOUcoolMODUL_task()
+{
+    if (!CONFIG_YOUcoolMODUL_ENABLED)
+    {
+        DEBUG_DEBUG(TAG, "YOUcoolMODUL Modul not Enabled");
+        return;
+    }
+    DEBUG_DEBUG(TAG, "Adding YOUcoolMODUL Task to Watchdog System...." );
+    xTaskCreate(
+        YOUcoolMODUL_task,
+        "YOUcoolMODUL_task",
+        2048,
+        nullptr,
+        5,
+        &YOUcoolMODUL_task_handle
+    );
+    esp_err_t result = esp_task_wdt_add(YOUcoolMODUL_task_handle);
+    if (result != ESP_OK)
+    {
+        DEBUG_ERROR(TAG, "Failed to add YOUcoolMODUL Task to Watchdog: %s", esp_err_to_name(result));
+        return;
+    }
+    DEBUG_INFO(TAG, "Adding YOUcoolMODUL Task to Watchdog System successful" );
+}*/
+
+void watchdog_tasks_init()
+{
+    main_task();
+    wachdog_task();
+    debug_task();
+    //YOUcoolMODUL_task();  
 }
